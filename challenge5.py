ba21e1234ea174dc3df07645558739181c9d7568
Muktar Yakub <rmubez@gmail.com>
2026-07-10 13:16:23 +0200

    Fixed space

--- challenge5.py
+++ challenge5.py
@@ -7,7 +7,11 @@ print (f"Lower: {full_name}".lower())
 #count = len(full_name)
 #print(f"Letters:{count}")
 
-print(f"Letters: {len(full_name)}")
+#print(f"Letters: {len(full_name)}")
+
+letters = full_name.replace(" ", "")
+print(f"Letters: {len(letters)}")
+
 
 print(f"First: {full_name.capitalize()[0]}")
 
